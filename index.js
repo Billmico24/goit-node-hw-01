5a4aaa6d331411cb6a2f1162ecdac5faf1c1c742
Billmico24 <138019098+Billmico24@users.noreply.github.com>
2024-06-20 21:54:31 +0800

    Commit all

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-import { listContacts, getContactById, addContact, removeContact } from './contacts.mjs';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-
-const argv = yargs(hideBin(process.argv)).argv;
+const contacts = require('./contacts.js');
+const argv = require('yargs').argv;
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      const allContacts = await listContacts();
+      const allContacts = await contacts.listContacts();
       console.table(allContacts);
       break;
 
     case 'get':
       if (id) {
-        const contact = await getContactById(id);
+        const contact = await contacts.getContactById(id);
         console.log(contact ? `Contact with ID ${id}:` : 'Contact not found');
         console.log(contact);
       } else {
@@ -23,7 +20,7 @@ async function invokeAction({ action, id, name, email, phone }) {
 
     case 'add':
       if (name && email && phone) {
-        await addContact(name, email, phone);
+        await contacts.addContact(name, email, phone);
         console.log(`Added contact: ${name}`);
       } else {
         console.error('Please provide name, email, and phone');
@@ -32,7 +29,7 @@ async function invokeAction({ action, id, name, email, phone }) {
 
     case 'remove':
       if (id) {
-        await removeContact(id);
+        await contacts.removeContact(id);
         console.log(`Removed contact with ID ${id}`);
       } else {
         console.error('Please provide a valid ID');
